4fafe88b4354ccbcd05ae834dfe583d431793185
Luca Perone <luca.perone@jooce.ch>
2022-08-09 15:22:30 +0200

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
+import { Grid } from "@mui/material"
+import { Route, Routes } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Favourites from "./routes/Favourites"
+import Friends from "./routes/Friends"
+import Lists from "./routes/Lists"
+import Network from "./routes/Network"
 
 const darkTheme = createTheme({
     palette: {
@@ -11,7 +19,19 @@ function App() {
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline />
-            <main>This app is using the dark mode</main>
+            <Grid container spacing={0}>
+                <Grid item xs="auto">
+                    <Navbar />
+                </Grid>
+                <Grid item xs>
+                    <Routes>
+                        <Route path="/" element={<Network />} />
+                        <Route path="/lists" element={<Lists />} />
+                        <Route path="/favourites" element={<Favourites />} />
+                        <Route path="/friends" element={<Friends />} />
+                    </Routes>
+                </Grid>
+            </Grid>
         </ThemeProvider>
     )
 }
