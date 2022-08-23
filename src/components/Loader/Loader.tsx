59a50fecc38acc2c425323cdb611277360d5e14c
Luca Perone <luca.perone@jooce.ch>
2022-08-23 11:36:57 +0200

    Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { Box } from "@mui/material"
+import { PacmanLoader } from "react-spinners"
+
+const Loader = () => {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+            }}
+        >
+            <PacmanLoader loading={true} color="#ff0046" />
+        </Box>
+    )
+}
+
+export default Loader
